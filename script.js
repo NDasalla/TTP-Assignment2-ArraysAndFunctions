938c2d0fba596101d3ef10922608a31c0206d9a5
Nikko D <ndasalla1323@gmail.com>
2023-06-16 00:19:30 -0400

    finished all methods, misc problem next?

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
 /* forEach(): iterates through each element in "element" array
 and executes a function on each element. Returns undefined*/
-const forEachFunc = (element, func) => {
-  const ret = [...element];
+const myEach = (element, func) => {
+  const arr = [...element];
   for (let i = 0; i < element.length; i++) {
-    func(ret[i]);
+    func(arr[i]);
   }
 };
+const arr1 = [1, 2, 3, 4, 5];
+let squared = 1;
+const tempForEach = (e) => {
+  return (squared *= e);
+};
+console.log(`forEach(): ${myEach(arr1, tempForEach)} | squared = ${squared}`);
 
+////////////////////////////////////////////////////////////////
 /*map(): iterates through each element in "element" array
 and executes a function on each element. Returns a new array*/
-const mapFunc = (element, func) => {
-  const ret = [...element];
+const myMap = (element, func) => {
+  const ret = [];
+  const compare = [...element];
   for (let i = 0; i < element.length; i++) {
-    ret[ret.length] = func(ret[i]);
+    ret[ret.length] = func(compare[i]);
   }
   return ret;
 };
+//adds 5
+const tempMap = (e) => {
+  return e + 5;
+};
+const arr2 = [...arr1];
+console.log(`map(): ${myMap(arr2, tempMap)}`);
 
+////////////////////////////////////////////////////////////////
 /*filter(): iterates through each element in "element" array
 and executes a function on each element and checks if the element
 returns true when passed into provided function. If true, element
 is pushed into the "ret" array. The "ret" array is returned after
 all elements are iterated through.*/
-const filterFunc = (element, func) => {
+const myFilter = (element, func) => {
   const ret = [];
-  const compareArr = [...element];
+  const compare = [...element];
   for (let i = 0; i < element.length; i++) {
-    if (!!func(compareArr[i])) ret[ret.length] = compareArr[i];
+    if (!!func(compare[i])) ret[ret.length] = compare[i];
   }
   return ret;
 };
 //filter test function
 const tempFilter = (e) => {
   if (e < 5) {
-    console.log(e);
+    // console.log(e);
     return true;
   } else return false;
 };
+const arr3 = [2, 4, 6, 8, 10, 5, 1];
+console.log(`filter(): ${myFilter(arr3, tempFilter)}`);
 
+////////////////////////////////////////////////////////////////
 /*some(): iterates through each element in "element" array until 
 an element satisfies the given function. Returns true when at least
 one element satisfies the given function.*/
-const someFunc = (element, func) => {
+const mySome = (element, func) => {
   const compareArr = [...element];
   for (let i = 0; i < element.length; i++) {
     if (!!func(compareArr[i])) return true;
@@ -50,23 +68,30 @@ const someFunc = (element, func) => {
 };
 //some test function
 const tempSome = (e) => {
-  if (e < 5) {
-    console.log(e);
+  if (e > 5) {
+    // console.log(e);
     return true;
   } else return false;
 };
+const arr4 = [...arr3];
+const arr4_1 = [1, 2, 3, 4, 5, 2, -1];
+console.log(
+  `some(): arr4 = ${mySome(arr4, tempSome)} | arr4_1 = ${mySome(
+    arr4_1,
+    tempSome
+  )}`
+);
 
+////////////////////////////////////////////////////////////////
 /*every(): iterates through each element in "element" array until 
-an element satisfies the given function. Returns true when at least
-one element satisfies the given function.*/
-const everyFunc = (element, func) => {
+an element satisfies the given function. Returns true when every 
+element satisfies the given function.*/
+const myEvery = (element, func) => {
   const temp = [];
   const compareArr = [...element];
   for (let i = 0; i < element.length; i++) {
     if (!!func(compareArr[i])) temp[temp.length] = compareArr[i];
   }
-  console.log(temp);
-  console.log(compareArr);
   //use of .length is not the best when comparing arrays,
   //but I do not know how else to compare them
   if (temp.length === compareArr.length) return true;
@@ -79,31 +104,52 @@ const tempEvery = (e) => {
     return true;
   } else return false;
 };
+const arr5 = [1, 3, -1, 4, 4, 2];
+const arr5_1 = [1, 2, 3, 4, 5, 2, -1, 10];
+console.log(
+  `every(): arr5 = ${myEvery(arr5, tempEvery)} | arr5_1 = ${myEvery(
+    arr5_1,
+    tempEvery
+  )}`
+);
 
-/*reduce(): iterates through each element in "element" array and 
-adds the value of the element into the "ret" variable. After for loop
-finishes, returns "ret" variable.*/
-const reduceFunc = (element, initialVal) => {
+////////////////////////////////////////////////////////////////
+/*reduce(): Initialize ret = first element of passed in array.
+Check if there is an initial value passed in. ret is the accumulator
+. After for loop finishes, returns "ret" variable.*/
+const myReduce = (element, func, initialVal) => {
   const arr = [...element];
-  let ret;
+  let ret = arr[0];
   if (!!initialVal || initialVal === 0) {
     ret = initialVal;
     for (let i = 0; i < element.length; i++) {
-      ret += arr[i];
+      ret = func(ret, arr[i]);
     }
   } else {
-    ret = arr[0];
     for (let i = 0; i < element.length; i++) {
-      ret += arr[i];
+      ret = func(ret, arr[i]);
     }
   }
   return ret;
 };
+const tempReduce = (a, b) => {
+  return a * 2 + b * 1;
+};
+const arr6 = [1, 3, -2];
+const arr6_1 = [1, 2, 3];
+console.log(
+  `reduce(): arr6 = ${myReduce(arr6, tempReduce, 1)} | arr6_1 = ${myReduce(
+    arr6_1,
+    tempReduce,
+    1
+  )}`
+);
 
+////////////////////////////////////////////////////////////////
 /*includes(): iterates through each element in "element" array until 
 an element satisfies the given function. Returns true when at least
 one element satisfies the given function.*/
-const includesFunc = (element, find, func) => {
+const myIncludes = (element, find, func) => {
   const compareArr = [...element];
   for (let i = 0; i < element.length; i++) {
     if (!!func(compareArr[i], find)) return true;
@@ -113,21 +159,24 @@ const includesFunc = (element, find, func) => {
 //some test function
 const tempIncludes = (a, b) => {
   if (a === b) {
-    console.log(`${a} & ${b} TRUE`);
+    // console.log(`${a} & ${b} TRUE`);
     return true;
   } else {
-    console.log(`${a} & ${b} FALSE`);
+    // console.log(`${a} & ${b} FALSE`);
     return false;
   }
 };
+const arr7 = ["hello", "good", "morning"];
+console.log(`includes(): arr7 = ${myIncludes(arr7, "gello", tempIncludes)}`);
 
+////////////////////////////////////////////////////////////////
 /*indexOf(): First, checks if a "begin" index is provided,
 where to start iterating. If a beginning index is provided
 start for loop from that index. Else, begin from index 0. 
 For each iteration, compare the element at that index with the
 element you are trying to find, "find." If found, return "i", 
-which is the index. If element is not found, return null.*/
-const indexOfFunc = (element, find, begin) => {
+which is the index. If element is not found, returns "Element Not Found".*/
+const myIndexOf = (element, find, begin) => {
   const compareArr = [...element];
   if (!!begin) {
     for (let i = begin; i < element.length; i++) {
@@ -138,9 +187,72 @@ const indexOfFunc = (element, find, begin) => {
       if (compareArr[i] === find) return i;
     }
   }
-  return null;
+  return "Element Not Found";
 };
+const arr8 = ["hello", "good", "morning"];
+console.log(`indexOf(): arr8 = ${myIndexOf(arr8, "morning", 0)}`);
 
-const arr = ["h", "e", "l", "l", "o"];
-//1st parameter = array, 2nd parameter = function
-console.log(indexOfFunc(arr, "v"));
+////////////////////////////////////////////////////////////////
+/*push(): Copies passed in array into array, arr. Appends
+"elementToAdd" into the end of "arr". Returns "arr"*/
+const myPush = (element, elementToAdd) => {
+  const arr = [...element];
+  arr[arr.length] = elementToAdd;
+  return arr;
+};
+const arr9 = ["hello", "good", "morning"];
+console.log(`push(): arr9 = ${myPush(arr9, "boink")}`);
+
+////////////////////////////////////////////////////////////////
+/*lastIndexOf(): Copies passed in array into array, compareArr. Initialize
+indexTracker = 0. Loop through array and if current element is equal
+to elementToFind, set indexTracker equal to the current index. This continues
+until the end of the array. Returns indexTracker.*/
+const myLastIndexOf = (element, elementToFind, begin) => {
+  const compareArr = [...element];
+  let indexTracker = "Element Not Found";
+  if (!!begin) {
+    for (let i = begin; i < element.length; i++) {
+      if (compareArr[i] === elementToFind) indexTracker = i;
+    }
+  } else {
+    for (let i = 0; i < element.length; i++) {
+      if (compareArr[i] === elementToFind) indexTracker = i;
+    }
+  }
+  return indexTracker;
+};
+const arr10 = ["hello", "good", "morning"];
+console.log(`lastIndexOf(): arr10 = ${myLastIndexOf(arr10, "good", 2)}`);
+
+////////////////////////////////////////////////////////////////
+/*Object.keys(): Added a new method to the Object data type using .prototype.
+Inside the function, initialized an empty array for appending the keys.
+In the for loop, iterates through each element in passed object and appends
+the keys. Returns the array of keys.*/
+Object.prototype.grabKeys = (obj) => {
+  const ret = [];
+  for (const key in obj) {
+    ret[ret.length] = key;
+    // console.log(ret);
+  }
+  return ret;
+};
+let myObj1 = { a: "x", b: "y", c: "z" };
+console.log(Object.grabKeys(myObj1));
+
+////////////////////////////////////////////////////////////////
+/*Object.keys(): Added a new method to the Object data type using .prototype.
+Inside the function, initialized an empty array for appending the keys.
+In the for loop, iterates through each element in passed object and appends
+the keys. Returns the array of keys.*/
+Object.prototype.grabValues = (obj) => {
+  const ret = [];
+  for (const key in obj) {
+    ret[ret.length] = obj[key];
+    // console.log(ret);
+  }
+  return ret;
+};
+let myObj2 = { a: "x", b: "y", c: "z" };
+console.log(Object.grabValues(myObj2));
